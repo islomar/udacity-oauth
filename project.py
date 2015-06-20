7e4224a909fd3e06ca61c1435c58f3f3cbe8edcb
islomar <islomar@gmail.com>
2015-06-20 14:14:27 +0200

    Create anti-forgery state token

--- project.py
+++ project.py
@@ -15,6 +15,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Create a state token to prevent request forgery.
+# Store it in a session for later validatioin.
+@app.route('/login')
+def showLogin():
+  state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+  login_session['state'] = state
+  return 'The current session state is %s' %login_session['state']
+
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
