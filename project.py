e82c695da83dad7b281d72cf610a2953b8888e25
islomar <islomar@gmail.com>
2015-06-24 21:13:09 +0200

    Add disconnect capabilities (it doesn't work :-( )

--- project.py
+++ project.py
@@ -70,6 +70,8 @@ def gconnect():
     response.headers['Content-Type'] = 'application/json'
     return response
 
+  print "Milestone 1"
+
   # Verify that the access token is valid for this app.
   if result['issued_to'] != CLIENT_ID:
     response = make_response(json.dumps("Token's client ID doesn't match app's.", 401))
@@ -77,10 +79,13 @@ def gconnect():
     response.headers['Content-Type'] = 'application/json'
     return response
 
+  print "The access token is valid for this app"
+
   # Check to see if user is already logged in
   stored_credentials = login_session.get('credentials')
   stored_gplus_id = login_session.get('gplus_id')
   if stored_credentials is not None and gplus_id == stored_gplus_id:
+    print "Oh oh... Current user is already connected."
     response = make_response(json.dumps("Current user is already connected.", 200))
     response.headers['Content-Type'] = 'application/json'
     return response
@@ -89,10 +94,14 @@ def gconnect():
   login_session['credentials'] = credentials
   login_session['gplus_id'] = gplus_id
 
+  print "Access token successfully stores in the session for later use."
+
   # Get user info
   userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
   params = {'access_token': credentials.access_token, 'alt': 'json'}
   answer = requests.get(userinfo_url, params=params)
+  
+  print "User info successfully recovered."
 
   data = answer.json()
 
@@ -101,6 +110,7 @@ def gconnect():
   login_session['username'] = data['name']
   login_session['picture'] = data['picture']
   login_session['email'] = data['email']
+  print "login_session: %s" % login_session
 
   output = ''
   output += '<h1>Welcome, '
@@ -113,6 +123,42 @@ def gconnect():
   flash("you are now logged in as %s" % login_session['username'])
   print "done!"
   return output
+
+# Disconnect - revoke a current user's token and reset their login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+  # Only disconnect a connected user.
+  credentials = login_session.get('credentials')
+  if credentials is None:
+    response = make_response(json.dumps("Current user not connected.", 401))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+  # Execute HTTP GET request to revoke current token
+  access_token = credentials.access_token
+  print "Token to be revoked %s" % access_token
+  url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+  h = httplib2.Http()
+  result = h.request(url, 'GET')[0]
+
+  if result['status'] == 200:
+    # Reset the user's session
+    del login_session['credentials']
+    del login_session['gplus_id']
+    del login_session['username']
+    del login_session['email']
+    del login_session['picture']
+
+    response = make_response(json.dumps("Successfully disconnected", 200))
+    response.headers['Content-Type'] = 'application/json'
+    print "Token successfully revoked: successfully disconnected"
+    return response
+  else:
+    # For whatever reason, the given token was invalid.
+    response = make_response(json.dumps("Failed to revoke token for given user.", 400))
+    response.headers['Content-Type'] = 'application/json'
+    print "Failed to revoke token for given user."
+    return response
+
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
